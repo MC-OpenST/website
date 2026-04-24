8084add1d83ec50f825cc3a9766ef320668e94f4
Linvin <>
2026-04-25 00:09:55 +0200

    修改Portal ui

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
   server: {
     host: '0.0.0.0',
     port: 4000,
     open: true,
     cors: true
   },
-  rollupOptions: {
-    input: {
-      upload: 'upload.html',
-      archive: 'archive.html'
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        upload: 'upload/upload.html',
+        archive: 'archive.html'
+      }
     }
   }
 })
